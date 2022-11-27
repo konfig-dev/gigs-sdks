a1975d4387ebdb9d10c656642ebfa5cd5598db41
dphuang2 <dylan.p.huang@gmail.com>
2022-11-26 23:30:20 -0800

    update test

--- typescript/src/index.test.ts
+++ typescript/src/index.test.ts
@@ -1,6 +1,8 @@
+import { GigsClient } from "./GigsClient";
+
 describe("getting started", () => {
   it("basic use case", () => {
-    
-
+    const client = new GigsClient()
+    client.users.usersCreate("myProject", {email: "test@test.com"})
   });
 });
